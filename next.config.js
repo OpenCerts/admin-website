a30649a6cb44e5fe63e2aed5a96ba962461c652a
yehjxraymond <yehjxraymond@gmail.com>
2018-10-10 13:57:14 +0800

    Feature/admin subdomain (#41)
    * Added cname and removed exampleIssuers.json
    
    * Fixed url path issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   exportPathMap: function exportMap() {
     return {
       "/": { page: "/" }
     };
   },
-  assetPrefix: isProd ? "/certificate-web-ui" : ""
+  assetPrefix: ""
 };
