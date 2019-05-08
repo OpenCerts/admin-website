e55f097a3bc8488835b41a785c555f6da51dc3a0
Jason Kam <thefiend@hotmail.sg>
2019-05-08 15:34:09 +0800

    Added Button component

---
+++ components/Button/index.js
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import styles from "./style.scss";
+
+const Button = ({
+  children,
+  primary,
+  danger,
+  buttonOutline,
+  primaryOutline,
+  dangerOutline,
+  onClick,
+  ...rest
+}) => {
+  const classes = classNames.bind(styles);
+  const defaultClassName = classes("button", {
+    primary,
+    danger,
+    buttonOutline,
+    primaryOutline,
+    dangerOutline
+  });
+  return (
+    <button className={defaultClassName} onClick={onClick} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  className: PropTypes.object,
+  onClick: PropTypes.func,
+  style: PropTypes.object,
+  rest: PropTypes.object,
+  primary: PropTypes.bool,
+  danger: PropTypes.bool,
+  buttonOutline: PropTypes.bool,
+  primaryOutline: PropTypes.bool,
+  dangerOutline: PropTypes.bool
+};
