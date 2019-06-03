9ea93161a65577b11cdf75e4fba0a73da1dc003f
Jason Kam <thefiend@hotmail.sg>
2019-06-03 10:52:36 +0800

    Added package and updated configs for scss imports

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-module.exports = {
+const withSass = require("@zeit/next-sass");
+
+module.exports = withSass({
+  analyzeBrowser: ["browser"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  },
+  cssModules: true,
   exportPathMap: function exportMap() {
     return {
       "/": { page: "/" }
     };
   },
-  assetPrefix: ""
-};
+  // Variables passed to both server and client
+  publicRuntimeConfig: {
+    network: process.env.NET
+  }
+});
