98a350a99fed4e2c12512cd2b19e5e01168f02c5
Jason Kam <thefiend@hotmail.sg>
2019-06-04 23:04:54 +0800

    Added colour variables

---
+++ src/styles/variables.js
@@ -0,0 +1,24 @@
+const variables = {
+  black: "#000000",
+  white: "#ffffff",
+  brandBlue: "#0099cc",
+  brandDarkBlue: "#005f7f",
+  brandOrange: "#ff9933",
+  brandDarkOrange: "#ff6a33",
+  lightGrey: "#e5e5e5",
+  greyBlue: "#c1c9d1",
+  brandDark: "#324353",
+  faintBlue: "#f5f8fb",
+  faintGreen: "#f5fbf7",
+
+  headerUiBg: "#f3f8fc",
+  lowTranslucencyWhite: "rgba(255, 255, 255, 0.7)",
+
+  successColor: "#28a745",
+  warningColor: "#ffbf00",
+  warningBackground: "#fff9e8",
+  invalidColor: "#e46767",
+  invalidBackground: "#fbebe7"
+};
+
+export default variables;
