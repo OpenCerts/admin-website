e493e2d5925d379c053fb31b7b95058a42941caf
Jason Kam <thefiend@hotmail.sg>
2019-06-17 12:09:16 +0800

    Fixed Hash Color Input variant

--- src/components/UI/HashColorInput.js
+++ src/components/UI/HashColorInput.js
@@ -6,37 +6,43 @@ import Input from "./Input";
 const VALIDATIONS = {
   address: { size: 42, maxLength: 50, validityFn: isValidAddress },
   hash: {
-    size: 60,
+    size: 66,
     maxLength: 80,
     validityFn: address => /^(0x){1}[0-9a-fA-F]{64}$/i.test(address)
   },
   none: { size: 70, maxLength: 99999, validityFn: () => true }
 };
 
-const HashColorInput = props => {
-  const { size, maxLength, validityFn } = VALIDATIONS[props.type]; // eslint-disable-line
-  const isValid = validityFn(props.value);
+const HashColorInput = ({ variant, type, placeholder, onChange, value }) => {
+  const { size, maxLength, validityFn } = VALIDATIONS[type]; // eslint-disable-line
+  const isValid = validityFn(value);
 
   return (
-    <HashColor hashee={props.value} clickable={false} color={isValid}>
+    <HashColor hashee={value} clickable={false} color={isValid}>
       <Input
         type="text"
-        onChange={props.onChange}
+        variant={variant}
+        onChange={onChange}
         size={size}
-        value={props.value}
+        value={value}
         spellCheck="false"
         style={{
           color: "inherit",
+          fontFamily: "var(--font-monospace) monospace",
+          fontSize: "var(--font-monospace-size)",
           border: isValid ? "solid 1px black" : "solid 1px #e7040f"
         }}
         maxLength={maxLength}
-        placeholder={props.placeholder}
+        placeholder={placeholder}
       />
     </HashColor>
   );
 };
 
+export default HashColorInput;
+
 HashColorInput.propTypes = {
+  variant: PropTypes.string,
   type: PropTypes.string,
   value: PropTypes.any,
   onChange: PropTypes.func,
@@ -46,5 +52,3 @@ HashColorInput.propTypes = {
 HashColorInput.defaultProps = {
   type: "none"
 };
-
-export default HashColorInput;
