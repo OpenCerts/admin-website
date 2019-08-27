9bdf173af075917a2c7aae30d0e50c8fcdef5677
Jason Kam <thefiend@hotmail.sg>
2019-08-27 16:29:31 +0800

    Increase border radius to 8px

--- src/components/UI/Panel/index.js
+++ src/components/UI/Panel/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 const base = css`
   background-color: white;
-  border-radius: 3px;
+  border-radius: 8px;
   border: none;
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
   display: block;
